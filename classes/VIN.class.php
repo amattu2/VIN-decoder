fe997ff718ae2d3bd46fd8e657be299b1cad3103
Alec M <alecmattu1234@gmail.com>
2021-08-19 09:50:07 -0400

    Implement (untested) model_year function

--- classes/VIN.class.php
+++ classes/VIN.class.php
@@ -19,14 +19,16 @@ class DecodedYearMismatch extends \Exception {}
 // Vehicle Identification Number Class
 class VIN {
   // Class Variables
-  protected $VIN = "";
-  protected $Year = 0;
-  protected $Country = "";
-  protected $Region = "";
+  protected $VIN = null;
+  protected $model_year = null;
   const VALID_CHARACTERS = "ABCDEFGHJKLMNPRSTUVWXYZ1234567890";
 
   /**
    * Class Constructor
+   *
+   * NOTE:
+   *   (1) Vehicles prior to 1981 are officially
+   *   unsupported, but might work by luck.
    *
    * @param string VIN
    * @throws None
@@ -35,7 +37,6 @@ class VIN {
    */
   public function __construct(string $vin)
   {
-    // Variables
     $this->VIN = strtoupper($vin);
   }
 
@@ -99,6 +100,37 @@ class VIN {
     return true;
   }
 
+  /**
+   * Decode VIN to Model Year
+   *
+   * NOTE:
+   *   (1) A null return value is
+   *   indicative of an invalid VIN.
+   *
+   * @return ?int model year
+   * @throws None
+   * @author Alec M. <https://amattu.com>
+   * @date 2021-08-19
+   */
+  public function model_year() : ?int
+  {
+    // Check VIN
+    if (!$this->valid())
+      return null;
+
+    // Check Existing Result
+    if ($this->model_year)
+      return $this->model_year;
+
+    // Decode VIN
+    if ($this->model_year = $this->vin_to_year())
+      if (!is_numeric($this->VIN[6]))
+        $this->model_year += 30;
+
+    // Return
+    return $this->model_year;
+  }
+
   public function country() : string
   {
     return "";
@@ -109,11 +141,6 @@ class VIN {
     return "";
   }
 
-  public function year() : int
-  {
-    return 0;
-  }
-
   public function manufacturer() : string
   {
     return "";
@@ -121,6 +148,83 @@ class VIN {
 
   public function model() : string
   {
+    return "";
+  }
 
+  /**
+   * Decode a VIN to model year
+   *
+   * @return ?int model year (1980-2010)
+   * @throws TypeError
+   * @author Alec M. <https://amattu.com>
+   * @date 2021-08-19
+   */
+  private function vin_to_year() : ?int
+  {
+    switch ($this->VIN[9]) {
+      case "A":
+        return 1980;
+      case "B":
+        return 1981;
+      case "C":
+        return 1982;
+      case "D":
+        return 1983;
+      case "E":
+        return 1984;
+      case "F":
+        return 1985;
+      case "G":
+        return 1986;
+      case "H":
+        return 1987;
+      case "J":
+        return 1988;
+      case "K":
+        return 1989;
+      case "L":
+        return 1990;
+      case "M":
+        return 1991;
+      case "N":
+        return 1992;
+      case "P":
+        return 1993;
+      case "R":
+        return 1994;
+      case "S":
+        return 1995;
+      case "T":
+        return 1996;
+      case "V":
+        return 1997;
+      case "W":
+        return 1998;
+      case "X":
+        return 1999;
+      case "Y":
+        return 2000;
+      case "1":
+        return 2001;
+      case "2":
+        return 2002;
+      case "3":
+        return 2003;
+      case "4":
+        return 2004;
+      case "5":
+        return 2005;
+      case "6":
+        return 2006;
+      case "7":
+        return 2007;
+      case "8":
+        return 2008;
+      case "9":
+        return 2009;
+    }
+
+    // Default
+    return null;
   }
 }
