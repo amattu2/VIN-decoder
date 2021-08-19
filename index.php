1f2a82505ebea00dd334070c8635b6439476c4e2
Alec M <alecmattu1234@gmail.com>
2021-08-19 10:45:55 -0400

    Tabs to spaces, add more documentation references

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
 /*
-	Produced 2019
-	By https://github.com/amattu2
-	Copy Alec M.
-	License GNU Affero General Public License v3.0
+  Produced 2019-2021
+  By https://github.com/amattu2
+  Copy Alec M.
+  License GNU Affero General Public License v3.0
 */
 
 /*
-	Notes:
-	- https://www.decodethevin.com/
-	- https://www.northamericanmotoring.com/forums/vindecoder.php
-	- https://stackoverflow.com/questions/6206954/iso-3779-vehicle-vin-decoder-in-php
+  Notes:
+  - https://www.decodethevin.com/
+  - https://www.northamericanmotoring.com/forums/vindecoder.php
+  - https://stackoverflow.com/questions/6206954/iso-3779-vehicle-vin-decoder-in-php
+  - https://en.wikipedia.org/wiki/Vehicle_identification_number
 */
 
 // Files
